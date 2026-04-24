ac8574be837ec107a6ae27fdaaa4f39fc0bb0732
bwhtsu <liliiyazh5@gmail.com>
2026-04-24 20:15:35 +0300

    Updates

--- goit-teme9.e740cfac.js
+++ goit-teme9.e740cfac.js
@@ -749,193 +749,142 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 //     }
 // });
 // =================================================
-const movies = [
-    {
-        id: 1,
-        title: "Inception",
-        year: 2010,
-        genre: [
-            "Sci-Fi",
-            "Action",
-            "Thriller"
-        ],
-        rating: 8.8,
-        duration: 148,
-        director: "Christopher Nolan",
-        cast: [
-            "Leonardo DiCaprio",
-            "Joseph Gordon-Levitt"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 2,
-        title: "The Dark Knight",
-        year: 2008,
-        genre: [
-            "Action",
-            "Crime",
-            "Drama"
-        ],
-        rating: 9.0,
-        duration: 152,
-        director: "Christopher Nolan",
-        cast: [
-            "Christian Bale",
-            "Heath Ledger"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 3,
-        title: "Interstellar",
-        year: 2014,
-        genre: [
-            "Sci-Fi",
-            "Drama"
-        ],
-        rating: 8.6,
-        duration: 169,
-        director: "Christopher Nolan",
-        cast: [
-            "Matthew McConaughey",
-            "Anne Hathaway"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 4,
-        title: "Titanic",
-        year: 1997,
-        genre: [
-            "Romance",
-            "Drama"
-        ],
-        rating: 7.9,
-        duration: 195,
-        director: "James Cameron",
-        cast: [
-            "Leonardo DiCaprio",
-            "Kate Winslet"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 5,
-        title: "Avatar",
-        year: 2009,
-        genre: [
-            "Sci-Fi",
-            "Adventure"
-        ],
-        rating: 7.8,
-        duration: 162,
-        director: "James Cameron",
-        cast: [
-            "Sam Worthington",
-            "Zoe Saldana"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 6,
-        title: "The Matrix",
-        year: 1999,
-        genre: [
-            "Sci-Fi",
-            "Action"
-        ],
-        rating: 8.7,
-        duration: 136,
-        director: "The Wachowskis",
-        cast: [
-            "Keanu Reeves",
-            "Laurence Fishburne"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 7,
-        title: "Forrest Gump",
-        year: 1994,
-        genre: [
-            "Drama",
-            "Romance"
-        ],
-        rating: 8.8,
-        duration: 142,
-        director: "Robert Zemeckis",
-        cast: [
-            "Tom Hanks",
-            "Robin Wright"
-        ],
-        isOscarWinner: true
-    },
-    {
-        id: 8,
-        title: "The Shawshank Redemption",
-        year: 1994,
-        genre: [
-            "Drama"
-        ],
-        rating: 9.3,
-        duration: 142,
-        director: "Frank Darabont",
-        cast: [
-            "Tim Robbins",
-            "Morgan Freeman"
-        ],
-        isOscarWinner: false
-    },
-    {
-        id: 9,
-        title: "Gladiator",
-        year: 2000,
-        genre: [
-            "Action",
-            "Drama"
-        ],
-        rating: 8.5,
-        duration: 155,
-        director: "Ridley Scott",
-        cast: [
-            "Russell Crowe",
-            "Joaquin Phoenix"
-        ],
-        isOscarWinner: true
-    }
-];
-const STORAGE_KEY = "dataMovie";
-const listEl = document.querySelector(".list");
-let currentMuvies = movies;
-const parseMovies = localStorage.getItem("STORAGE_KEY");
-if (parseMovies) {
-    currentMuvies = JSON.parse(parseMovies);
-    createItems(currentMuvies);
-}
-function createItems(array) {
-    const item = array.map(({ id, title, year, rating, director, isOscarWinner })=>{
-        return `
-        <li class="item" id="${id}">
-            <h2>\u{41D}\u{430}\u{437}\u{432}\u{430}: ${title}</h2>
-            <p>\u{420}\u{456}\u{43A}: ${year}</p>
-            <p>\u{420}\u{435}\u{439}\u{442}\u{438}\u{43D}\u{433}: ${rating}</p>
-            <h3>\u{420}\u{435}\u{436}\u{438}\u{441}\u{435}\u{440}: ${director}</h3>
-            <p>\u{41E}\u{441}\u{43A}\u{430}\u{440}: ${isOscarWinner ? "\u041E\u0442\u0440\u0438\u043C\u0430\u0432" : "\u041D\u0435 \u043E\u0442\u0440\u0438\u043C\u0430\u0432"}</p>
-            <button class="delit" type=""button>Delit</button>
-        </li>`;
-    }).join("");
-    listEl.innerHTML = item;
-}
-createItems(currentMuvies);
-listEl.addEventListener("click", (evt)=>{
-    if (evt.target.nodeName !== "BUTTON") return;
-    const id = Number(evt.target.closest("li").id);
-    const idx = currentMuvies.findIndex((muvie)=>muvie.id === id);
-    currentMuvies.splice(idx, 1);
-    localStorage.setItem("STORAGE_KEY", JSON.stringify(currentMuvies));
-    createItems(currentMuvies);
-});
+// const movies = [
+//   {
+//     id: 1,
+//     title: "Inception",
+//     year: 2010,
+//     genre: ["Sci-Fi", "Action", "Thriller"],
+//     rating: 8.8,
+//     duration: 148,
+//     director: "Christopher Nolan",
+//     cast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 2,
+//     title: "The Dark Knight",
+//     year: 2008,
+//     genre: ["Action", "Crime", "Drama"],
+//     rating: 9.0,
+//     duration: 152,
+//     director: "Christopher Nolan",
+//     cast: ["Christian Bale", "Heath Ledger"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 3,
+//     title: "Interstellar",
+//     year: 2014,
+//     genre: ["Sci-Fi", "Drama"],
+//     rating: 8.6,
+//     duration: 169,
+//     director: "Christopher Nolan",
+//     cast: ["Matthew McConaughey", "Anne Hathaway"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 4,
+//     title: "Titanic",
+//     year: 1997,
+//     genre: ["Romance", "Drama"],
+//     rating: 7.9,
+//     duration: 195,
+//     director: "James Cameron",
+//     cast: ["Leonardo DiCaprio", "Kate Winslet"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 5,
+//     title: "Avatar",
+//     year: 2009,
+//     genre: ["Sci-Fi", "Adventure"],
+//     rating: 7.8,
+//     duration: 162,
+//     director: "James Cameron",
+//     cast: ["Sam Worthington", "Zoe Saldana"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 6,
+//     title: "The Matrix",
+//     year: 1999,
+//     genre: ["Sci-Fi", "Action"],
+//     rating: 8.7,
+//     duration: 136,
+//     director: "The Wachowskis",
+//     cast: ["Keanu Reeves", "Laurence Fishburne"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 7,
+//     title: "Forrest Gump",
+//     year: 1994,
+//     genre: ["Drama", "Romance"],
+//     rating: 8.8,
+//     duration: 142,
+//     director: "Robert Zemeckis",
+//     cast: ["Tom Hanks", "Robin Wright"],
+//     isOscarWinner: true
+//   },
+//   {
+//     id: 8,
+//     title: "The Shawshank Redemption",
+//     year: 1994,
+//     genre: ["Drama"],
+//     rating: 9.3,
+//     duration: 142,
+//     director: "Frank Darabont",
+//     cast: ["Tim Robbins", "Morgan Freeman"],
+//     isOscarWinner: false
+//   },
+//   {
+//     id: 9,
+//     title: "Gladiator",
+//     year: 2000,
+//     genre: ["Action", "Drama"],
+//     rating: 8.5,
+//     duration: 155,
+//     director: "Ridley Scott",
+//     cast: ["Russell Crowe", "Joaquin Phoenix"],
+//     isOscarWinner: true
+//   }
+// ];
+// const STORAGE_KEY = "dataMovie";
+// const listEl = document.querySelector(".list");
+// let currentMuvies = movies;
+// const parseMovies = localStorage.getItem("STORAGE_KEY");
+// if(parseMovies) {
+//     currentMuvies = JSON.parse(parseMovies);
+//     createItems(currentMuvies);
+// }
+// function createItems(array) {
+//     const item = array.map(({id, title, year, rating, director, isOscarWinner}) =>{
+//         return `
+//         <li class="item" id="${id}">
+//             <h2>Назва: ${title}</h2>
+//             <p>Рік: ${year}</p>
+//             <p>Рейтинг: ${rating}</p>
+//             <h3>Режисер: ${director}</h3>
+//             <p>Оскар: ${isOscarWinner?"Отримав":"Не отримав"}</p>
+//             <button class="delit" type=""button>Delit</button>
+//         </li>`
+//     }).join("")
+//     listEl.innerHTML = item;
+// }
+// createItems(currentMuvies);
+// listEl.addEventListener("click", (evt) => {
+//     if(evt.target.nodeName !== "BUTTON") {
+//         return
+//     }
+//     const id = Number(evt.target.closest("li").id);
+//     const idx = currentMuvies.findIndex((muvie) => muvie.id === id);
+//     currentMuvies.splice(idx, 1);
+//     localStorage.setItem("STORAGE_KEY", JSON.stringify(currentMuvies));
+//     createItems(currentMuvies);
+// })
+// =================================================
+// Створіть програму для зберігання контактів — ім'я, прізвище, телефон та електронна адреса. Зберігайте контакти в localStorage та дозволяйте користувачу додавати, видаляти та редагувати контакти.
 
 },{}]},["cvTP8","a8vrg"], "a8vrg", "parcelRequire94bc", {})
 
